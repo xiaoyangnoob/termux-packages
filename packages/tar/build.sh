a56a226c43a375ad01246de4e116dc6bc40d29a1
xiaoyangnoob <xiaoyangnoob@outlook.com>
2024-10-04 12:03:27 +0800

    Tar static build.

--- packages/tar/build.sh
+++ packages/tar/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.35
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/tar/tar-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=4d62ff37342ec7aed748535323930c7cf94acf71c3591882b26a7ea50f3edc16
-TERMUX_PKG_DEPENDS="libandroid-glob, libiconv"
+TERMUX_PKG_DEPENDS="libandroid-glob-static, libiconv-static"
 TERMUX_PKG_ESSENTIAL=true
 
 # When cross-compiling configure guesses that d_ino in struct dirent only exists
@@ -16,6 +16,8 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="gl_cv_struct_dirent_d_ino=yes"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" ac_cv_func_mkfifoat=yes"
 
 termux_step_pre_configure() {
+    LDFLAGS+=" -static"
+    LDFLAGS+=" -ffunction-sections -fdata-sections -Wl,--gc-sections"
 	CPPFLAGS+=" -D__USE_FORTIFY_LEVEL=0"
 	LDFLAGS+=" -landroid-glob"
 	# https://android.googlesource.com/platform/bionic/+/master/docs/32-bit-abi.md#is-32_bit-on-lp32-y2038
