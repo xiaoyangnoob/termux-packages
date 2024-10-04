b382122231957515ea17b39aa0c4ea7c589ea2e9
xiaoyangnoob <xiaoyangnoob@outlook.com>
2024-10-04 12:09:24 +0800

    Use tabs for indentation. (Pass check)

--- packages/tar/build.sh
+++ packages/tar/build.sh
@@ -16,8 +16,8 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="gl_cv_struct_dirent_d_ino=yes"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" ac_cv_func_mkfifoat=yes"
 
 termux_step_pre_configure() {
-    LDFLAGS+=" -static"
-    LDFLAGS+=" -ffunction-sections -fdata-sections -Wl,--gc-sections"
+	LDFLAGS+=" -static"
+	LDFLAGS+=" -ffunction-sections -fdata-sections -Wl,--gc-sections"
 	CPPFLAGS+=" -D__USE_FORTIFY_LEVEL=0"
 	LDFLAGS+=" -landroid-glob"
 	# https://android.googlesource.com/platform/bionic/+/master/docs/32-bit-abi.md#is-32_bit-on-lp32-y2038
